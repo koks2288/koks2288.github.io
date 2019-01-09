7df2d84554e44ef6c0bbfba33a197a49c0269b3a
Danil <semenikhin88888@gmail.com>
2019-01-09 16:02:29 +0200

    commit

--- wificamera/link/header.php
+++ wificamera/link/header.php
@@ -42,30 +42,3 @@ $result = mail($to, $subject, $message, $headers);
 echo ("Обязательные поля не заполнены. Введите Телефон");
 }*/
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	
-	<link rel="stylesheet" href="/style/style.css">
-	<link rel="stylesheet" href="/style/styleModalWindow.css">
-	<!-- <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.css">
-	 --><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	
-	<link rel="stylesheet" href="/bower_components/fancybox-master/dist/jquery.fancybox.min.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
-</head>
-<body>
-	<div class="container-fluid header" id="header">
-		<div class="row">
-			<div class="col-xs-6 col-md-3">
-				<a  href="/index.php"><img src="/images/vigio.png" alt=""></a>
-			</div>
-			<div class="col-xs-6 col-md-6" >
-				<h2 style="text-transform: uppercase;">Готовые комплекты видеонаблюдения</h2>
-			</div>
-			<div class="col-xs-6 col-md-3">
-				<h2><b>Пн. -Сб.: с 9:00 до 18:00</b></h2>
-			</div>
-		</div>
-	</div> 
